1e8fae5c5187048d140712e7ba11d09bb35b8d43
Phaneesh Nagaraja <phaneesh.nagaraja@sungard.com>
2010-05-24 14:25:54 +0530

    Fixing two-way binding update event

--- zk-simple-twoway-databinding/grails-app/composers/zk/simple/twoway/databinding/IndexComposer.groovy
+++ zk-simple-twoway-databinding/grails-app/composers/zk/simple/twoway/databinding/IndexComposer.groovy
@@ -5,12 +5,24 @@ import org.zkoss.zul.Listbox
 
 //Index composer used to provide controller support for index.zul
 class IndexComposer extends GrailsComposer {
-
-    def lstSample
-
-    def persons
-
+ 
+    def selectedItem
+    
     def afterCompose = { window ->
-        persons = Person.list()
+        // initialize components here
+    }
+
+    //Get the list of person objects
+    //TODO: Service should be used to perform CRUD and all function calls
+    //Service dependency will be injected by Spring
+    public List<Person> getPersons() {
+        return Person.list()
+    }
+
+    //Save or Update the model back to the database when Update button is clicked on the UI
+    def onClick_doSubmit() {
+        persons.each { p->
+            p.save()
+        }
     }
 }
