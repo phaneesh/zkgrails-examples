009e235c2094a38ec462615ecd3a1f0b77481008
Phaneesh Nagaraja <phaneesh.nagaraja@sungard.com>
2010-06-07 16:46:18 +0530

    Refactored to print results into a textbox instead of console

--- zk-tips-tricks/grails-app/composers/zk/tips/tricks/IndexComposer.groovy
+++ zk-tips-tricks/grails-app/composers/zk/tips/tricks/IndexComposer.groovy
@@ -2,25 +2,22 @@ package zk.tips.tricks
 
 import org.zkoss.zkgrails.*
 import org.zkoss.zul.Window
+import org.zkoss.zul.Textbox
 
 class IndexComposer extends GrailsComposer {
 
     def Window parentWindow
-
+    def Textbox composers
     def afterCompose = { window ->
         parentWindow = window as Window
     }
 
     def onClick_getComposers() {
-        println("***********************************************")
-        println("Parent Composer : " +parentWindow.attributes[parentWindow.id +"\$composer"])
-        println("***********************************************")
-        println("Total Childern : $parentWindow.childern.size()")
+        composers.value = parentWindow.attributes[parentWindow.id +"\$composer"].toString() +"\n"
         parentWindow.children.each { f ->
             if(f instanceof Window) {
-                println("Embeded Composer : " +f.attributes[f.id +"\$composer"])
+                composers.value = composers.value +f.attributes[f.id +"\$composer"].toString() +"\n"
             }
         }
-        println("***********************************************")
     }
 }
