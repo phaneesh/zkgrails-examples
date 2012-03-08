6878c506cff695e16b66277725b99ab109f2d8bd
Phaneesh <phaneesh.n@gmail.com>
2012-03-08 14:17:35 +0530

    Update zk-simple-databinding/grails-app/domain/zk/simple/databinding/Person.groovy

--- zk-simple-databinding/grails-app/domain/zk/simple/databinding/Person.groovy
+++ zk-simple-databinding/grails-app/domain/zk/simple/databinding/Person.groovy
@@ -1,13 +1,4 @@
-/** *****************************************************************************
- © 2010 SunGard Higher Education.  All Rights Reserved.
 
- CONFIDENTIAL BUSINESS INFORMATION
-
- THIS PROGRAM IS PROPRIETARY INFORMATION OF SUNGARD HIGHER EDUCATION
- AND IS NOT TO BE COPIED, REPRODUCED, LENT, OR DISPOSED OF,
- NOR USED FOR ANY PURPOSE OTHER THAN THAT WHICH IT IS SPECIFICALLY PROVIDED
- WITHOUT THE WRITTEN PERMISSION OF THE SAID COMPANY
- ****************************************************************************** */
 package zk.simple.databinding
 
 class Person {
